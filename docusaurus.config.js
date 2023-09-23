fb73b76098d05bc23c00e9e5980d83933b42b40a
Wake Huang <mail@wakehuang.com>
2023-09-24 02:48:58 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://100hours.huanghaoxing.com/',
+  url: 'https://code.huanghaoxing.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
