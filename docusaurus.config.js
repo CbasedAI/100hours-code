bd90b6eb11c2506d7b8434af40e1914c48aa25de
Wake Huang <mail@wakehuang.com>
2023-09-24 00:30:59 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://python.huanghaoxing.com/',
+  url: 'https://code.huanghaoxing.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '100小时后请叫我Python程序员',
+        title: '《100小时后请叫我程序员》',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -74,7 +74,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: '教程',
+            label: 'Python教程',
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
